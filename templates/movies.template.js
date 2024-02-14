19eeb1c01c445352bc7cb445234d36f72c43871d
Pierre <contact@perfweb.net>
2024-02-14 10:34:50 +0100

    test

--- templates/movies.template.js
+++ templates/movies.template.js
@@ -2,7 +2,15 @@ var MoviesTemplate = `
 
 <div class="container">
     <div class="row">
-        <movie ng-repeat="movie in $ctrl.movies"></movie>
+        <div class="col-md-6" ng-repeat="movie in $ctrl.movies">
+        <div class="card mb-3">
+            <div class="card-body">
+                <!--<img src="{{ movie.image }}" class="card-img-top" alt="image à rajouté">-->
+                <h2 class="card-title">{{ movie.title }}</h2>
+                <p class="card-text">{{ movie.description }}</p>
+            </div>
+        </div>
+    </div>
     </div>
 </div>
 
