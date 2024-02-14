71a3d1488e90f32af65e56bfed8c14577ee9bf0a
Pierre <contact@perfweb.net>
2024-02-14 10:01:06 +0100

    test

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,17 +1,3 @@
-mvApp.controller('MovieController', function(){
-    this.movies = [
-        {
-            /*image: "image.jpg",*/
-            title: "Film title 1",
-            description: "Description 1",
-            grade: [
-                { star: 5 },
-                { star: 4 },
-                { star: 3 }
-            ],
-            comments: [
-                { user: "Bob", data: "Super" },
-                { user: "Jean", data: "Null" }
-            ]
-        }]
+mvApp.controller('MovieController', function(MoviesService){
+    this.movies = MoviesService.getMovies();
 })
